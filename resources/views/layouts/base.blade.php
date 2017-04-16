d87c75719f9b6fc97dc3e24821176bcee0cf73bd
fisayoafolayan <afolayanfisayo@gmail.com>
2017-04-16 14:43:06 +0100

    Added meta details

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,11 +1,32 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
     <head>
+        <head  itemscope itemtype="http://schema.org/WebSite">
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>News App</title>
+        <title>News Application with Laravel</title>
+        <link rel="icon" href="https://news-web-app.herokuapp.com/img/images.png">
+        <meta itemprop="name" content="News Application with Laravel"/>
+        <meta itemprop="url" content="https://news-web-app.herokuapp.com "/>
+        <meta name="description" content="A Laravel app that gathers top news across multiple news sites . All thanks to newsapi.org || Fisayo Afolayan">
+        <meta name="author" content="Fisayo Afolayan"> 
+        <meta name="keywords" content="developer, news, api, software developer, nigeria, africa, php, laravel">
+        <meta name="robots" content="index,follow">
+        <meta property="og:image" content="https://news-web-app.herokuapp.com/img/images.png"/>
+        <meta property="og:title" content="News Application with Laravel"/>
+        <meta property="og:type" content="website"/>
+        <meta name="og:description" content="A Laravel app that gathers top news across multiple news sites . All thanks to newsapi.org || Fisayo Afolayan">
+        <meta property="og:url" content="https://news-web-app.herokuapp.com "/>
+        <meta property="og:site_name" content="News Application with Laravel"/>
+        <meta name="twitter:site" content="@fisayoafolayan">
+        <meta name="twitter:creator" content="@fisayoafolayan">
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:title" content="News Application with Laravel">
+        <meta name="twitter:description" content="A Laravel app that gathers top news across multiple news sites . All thanks to newsapi.org || Fisayo Afolayan">
+        <meta name="twitter:image" content="https://news-web-app.herokuapp.com/img/images.png">
+        <meta name="twitter:domain" content="https://news-web-app.herokuapp.com">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet" type="text/css">
@@ -16,7 +37,7 @@
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://use.fontawesome.com/43db5e56d9.js"></script>
-        
+
     </head>
     <body>
     <div id="appenDivNews">
